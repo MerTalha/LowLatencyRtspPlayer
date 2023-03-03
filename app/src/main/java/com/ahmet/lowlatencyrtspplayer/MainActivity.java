f1acadbac6d08d55df6c5b193b7a3438f371b12c
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2023-03-03 20:41:50 +0300

    Camera change buttons add

--- app/src/main/java/com/ahmet/lowlatencyrtspplayer/MainActivity.java
+++ app/src/main/java/com/ahmet/lowlatencyrtspplayer/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.alexvas.rtsp.widget.RtspSurfaceView;
 
 public class MainActivity extends AppCompatActivity {
     RtspSurfaceView surfaceViewRight,surfaceViewLeft;
+    Button leftButton1,leftButton2,leftButton3,leftButton4,leftButtonRefresh,rightButton1,rightButton2,rightButton3,rightButton4,rightButtonRefresh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -17,18 +21,152 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         surfaceViewRight = findViewById(R.id.surfaceViewRight);
         surfaceViewLeft = findViewById(R.id.surfaceViewLeft);
+        leftButton1 = findViewById(R.id.left1);
+        leftButton2 = findViewById(R.id.left2);
+        leftButton3 = findViewById(R.id.left3);
+        leftButton4 = findViewById(R.id.left4);
+        leftButtonRefresh = findViewById(R.id.leftRefresh);
+        rightButton1 = findViewById(R.id.right1);
+        rightButton2 = findViewById(R.id.right2);
+        rightButton3 = findViewById(R.id.right3);
+        rightButton4 = findViewById(R.id.right4);
+        rightButtonRefresh = findViewById(R.id.righRefresh);
+
+
 
         Uri uri1 = Uri.parse("rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?");
-        Uri uri2 = Uri.parse("rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?");
-        Uri uri3 = Uri.parse("rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?");
-        Uri uri4 = Uri.parse("rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?");
-        Uri uri5 = Uri.parse("rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?");
+        Uri uri2 = Uri.parse("rtsp://192.168.1.10:554/user=admin&password=&channel=2&stream=0.sdp?");
+        Uri uri3 = Uri.parse("rtsp://192.168.1.10:554/user=admin&password=&channel=3&stream=0.sdp?");
+        Uri uri4 = Uri.parse("rtsp://192.168.1.10:554/user=admin&password=&channel=4&stream=0.sdp?");
+        Uri uri5 = Uri.parse("rtsp://192.168.1.10:554/user=admin&password=&channel=5&stream=0.sdp?");
 
         surfaceViewRight.init(uri1,"","");
         surfaceViewLeft.init(uri2,"","");
 
         surfaceViewRight.start(true, true);
         surfaceViewLeft.start(true,true);
+        leftButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (surfaceViewLeft.isStarted()){
+                    surfaceViewLeft.stop();
+                    Toast.makeText(MainActivity.this, "Left Camera 1 Stop and Start", Toast.LENGTH_SHORT).show();
+
+                }
+                surfaceViewLeft.init(uri1,"","");
+                surfaceViewLeft.start(true,true);
+            }
+        });
+        leftButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (surfaceViewLeft.isStarted()){
+                    surfaceViewLeft.stop();
+                    Toast.makeText(MainActivity.this, "Left Camera 2 Stop and Start", Toast.LENGTH_SHORT).show();
+
+                }
+                surfaceViewLeft.init(uri2,"","");
+                surfaceViewLeft.start(true,true);
+
+            }
+        });
+        leftButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (surfaceViewLeft.isStarted()){
+                    surfaceViewLeft.stop();
+                    Toast.makeText(MainActivity.this, "Left Camera 3 Stop and Start", Toast.LENGTH_SHORT).show();
+
+                }
+                surfaceViewLeft.init(uri3,"","");
+                surfaceViewLeft.start(true,true);
+            }
+        });
+        leftButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (surfaceViewLeft.isStarted()){
+                    surfaceViewLeft.stop();
+                    Toast.makeText(MainActivity.this, "Left Camera 4 Stop and Start", Toast.LENGTH_SHORT).show();
+
+                }
+                surfaceViewLeft.init(uri4,"","");
+                surfaceViewLeft.start(true,true);
+            }
+        });
+        leftButtonRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (surfaceViewLeft.isStarted()){
+                    surfaceViewLeft.stop();
+                    Toast.makeText(MainActivity.this, "Left Camera Stop and Refresh", Toast.LENGTH_SHORT).show();
+
+                }
+                surfaceViewLeft.start(true,true);
+            }
+        });
+
+
+        rightButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (surfaceViewRight.isStarted()){
+                    surfaceViewRight.stop();
+                    Toast.makeText(MainActivity.this, "Right Camera 1 Stop and Start", Toast.LENGTH_SHORT).show();
+
+                }
+                surfaceViewRight.init(uri1,"","");
+                surfaceViewRight.start(true,true);
+            }
+        });
+        rightButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (surfaceViewRight.isStarted()){
+                    surfaceViewRight.stop();
+
+                    Toast.makeText(MainActivity.this, "Right Camera 2 Stop and Start", Toast.LENGTH_SHORT).show();
+
+                }
+                surfaceViewRight.init(uri2,"","");
+                surfaceViewRight.start(true,true);
+            }
+        });
+        rightButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (surfaceViewRight.isStarted()){
+                    surfaceViewRight.stop();
+                    Toast.makeText(MainActivity.this, "Right Camera 3 Stop and Start", Toast.LENGTH_SHORT).show();
+
+                }
+                surfaceViewRight.init(uri3,"","");
+                surfaceViewRight.start(true,true);
+            }
+        });
+        rightButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (surfaceViewRight.isStarted()){
+                    surfaceViewRight.stop();
+                    Toast.makeText(MainActivity.this, "Right Camera 4 Stop and Start", Toast.LENGTH_SHORT).show();
+
+                }
+                surfaceViewRight.init(uri4,"","");
+                surfaceViewRight.start(true,true);
+            }
+        });
+        rightButtonRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (surfaceViewRight.isStarted()){
+                    surfaceViewRight.stop();
+                    Toast.makeText(MainActivity.this, "Right Camera  Stop and Refresh", Toast.LENGTH_SHORT).show();
+
+                }
+                surfaceViewRight.start(true,true);
+            }
+        });
 
     }
 }
